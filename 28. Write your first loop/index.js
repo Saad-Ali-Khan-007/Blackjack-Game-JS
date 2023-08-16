628bc649d6b33ae1b6e9367d475dc1f0def95069
Saad-Ali-Khan-007 <140542498+Saad-Ali-Khan-007@users.noreply.github.com>
2023-08-16 11:40:58 -0700

    JS Practice game

---
+++ 28. Write your first loop/index.js
@@ -0,0 +1,5 @@
+// Create a for loop that counts from 10 to 100 in steps of 10
+for (let count = 10; count <= 100; count += 10) {
+  console.log(count);
+}
+// Use console.log to log out the numbers
